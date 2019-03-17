8498c0ff39247c5ba37e68e42b15f3ffde30ddb4
fictio <hek@stpass.com>
2019-03-17 22:41:00 +0800

    fix: Task wait and notifyall;#190317

--- src/main/java/com/fictio/parrot/thinking/thread/WaxOMatic.java
+++ src/main/java/com/fictio/parrot/thinking/thread/WaxOMatic.java
@@ -1,13 +1,14 @@
 package com.fictio.parrot.thinking.thread;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
 
 class Car {
 	private boolean waxOn = false;
@@ -78,118 +79,124 @@ class WaxOff implements Runnable {
  * @author dk
  *
  */
+@Slf4j
 public class WaxOMatic {
 
-	private abstract class Food {
+	private abstract class Person {
 		String name;
-		public Food(String name) {
+		int blood;
+		public Person(String name) {
 			this.name = name;
+			this.blood = 100;
 		}
 	}
 	
-    class Cake extends Food {
-		String builder;
-		LocalDateTime buildTime;
-		LocalDateTime packTime;
-		LocalDateTime labelTime;
-		public Cake(String builder) {
-			super("cake");
-			this.builder = builder;
-			this.buildTime = LocalDateTime.now();
+    class Knight extends Person{
+		int attackPower;
+		boolean survive;
+		boolean go = false;
+		public Knight(String name) {
+			super(name);
+			survive = true;
+			attackPower = 10 + new Random().nextInt(20);
 		}
-		public synchronized void pack() {
-			OrnamentalGarden.sleep(1000);
-			this.packTime = LocalDateTime.now();
+		public synchronized void attack() {
+			log.info("Attack,{}",this);
+			go = true;
 			notifyAll();
 		}
-		public synchronized void label() {
-			this.labelTime = LocalDateTime.now();
+		public synchronized void defensive() {
+			log.info("Defensive,{}",this);
+			go = false;
 			notifyAll();
 		}
-		public synchronized void waitingForPack() {
-			if(packTime == null)
-				try {
-					wait();
-				} catch (InterruptedException e) {
-					System.out.println("Wating for pack failed!");
-					e.printStackTrace();
-				}
+		public synchronized void waitForAttack() throws InterruptedException {
+			while(go == false) {
+				log.info("wait for attack,{}",this);
+				wait();
+			}
 		}
-		public synchronized void waitingForLabel() {
-			if(labelTime == null)
-				try {
-					wait();
-				} catch (InterruptedException e) {
-					System.out.println("Wating for label failed!");
-					e.printStackTrace();
-				}
+		public synchronized void waitForDefensive() throws InterruptedException {
+			while(go == true) {
+				log.info("wait for defensive,{}",this);
+				wait();
+			}
 		}
 		@Override
 		public String toString() {
-			StringBuilder builder2 = new StringBuilder();
-			builder2.append("{builder:").append(builder).append(", buildTime:").append(buildTime).append(", packTime:")
-					.append(packTime).append(", labelTime:").append(labelTime).append("}");
-			return builder2.toString();
+			StringBuilder builder = new StringBuilder();
+			builder.append("{attackPower:").append(attackPower).append(", survive:").append(survive).append(", go:")
+					.append(go).append(", name:").append(name).append(", blood:").append(blood).append("}");
+			return builder.toString();
 		}
 	}
 	
-    // pack for cake
-	private class Packer implements Runnable {
-		private Cake cake;
-		public Packer(Cake cake) {
-			this.cake = cake;
+	private class Attack implements Runnable {
+		private Knight knightA;
+		@SuppressWarnings("unused")
+		private Knight knightB;
+		public Attack(Knight knightA,Knight knightB) {
+			this.knightA = knightA;
+			this.knightB = knightB;
 		}
 		public void run() {
-			if(cake.packTime == null) {
-				cake.pack();
-				cake.waitingForLabel();
+			while(!Thread.interrupted()) {
+				knightA.attack();
+				try {
+					knightA.waitForDefensive();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
 	
-	// label for cake,Cake need pack first
-	private class Labeler implements Runnable {
-		private Cake cake;
-		public Labeler(Cake cake) {
-			this.cake = cake;
+	private class Defensive implements Runnable {
+		private Knight knightA;
+		@SuppressWarnings("unused")
+		private Knight knightB;
+		public Defensive(Knight knightA,Knight knightB) {
+			this.knightA = knightA;
+			this.knightB = knightB;
 		}
-		@Override
 		public void run() {
-			if(cake.packTime == null) {
-				cake.waitingForLabel();
-			}else if(cake.packTime != null && cake.labelTime == null) {
-				cake.label();
+			while(!Thread.interrupted()) {
+				knightA.defensive();
+				try {
+					knightA.waitForAttack();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
-			
 		}
 	}
 	
 	List<String> waiters = Arrays.asList("Tom","Joe","Bin","Air","Alica","Ginduluman",
 			"Talibon","Tabilaran","Alon","Anda","Tibigon","Jao");
+
+	private int count = 0;
 	
-	private synchronized String getName(int id) {
+	private String getName(int id) {
 		return waiters.get(id);
+	}
+	
+	private int currentId() {
+		if(count > waiters.size()-1) count = 0;
+		else count++;
+		return count;
 	}
 	
 	@Test
 	public void test2() {
-		List<Cake> cakes = new ArrayList<>();
+		Knight k1 = new Knight(getName(currentId()));
+		Knight k2 = new Knight(getName(currentId()));
+		
 		ExecutorService exec = Executors.newCachedThreadPool();
-		int i = -1;
-		while(cakes.size() <= 10) {
-			if(i < waiters.size()-1 ) i++;
-			else i = 0;
-			Cake cake = new Cake(getName(i));
-			exec.execute(new Packer(cake));
-			exec.execute(new Labeler(cake));
-			cakes.add(cake);
-		}
-		OrnamentalGarden.sleep(3000);
-		for(Cake cake : cakes)
-			System.out.println(cake);
+		exec.execute(new Attack(k1, k2));
+		exec.execute(new Defensive(k1, k2));
 		
+		OrnamentalGarden.sleep(2000);
 		exec.shutdownNow();
-		
 	}
 	
 	
@@ -202,5 +209,62 @@ public class WaxOMatic {
 		OrnamentalGarden.sleep(6000);
 		exec.shutdownNow();
 	}
+	
+	
+	private class TaskA implements Runnable {
+		
+		public synchronized void waitPrint() throws InterruptedException {
+			log.info("Read for wait.");
+			wait();
+			log.info("Yes, I Got it.");
+		}
+		
+		public synchronized void awake() {
+			notifyAll();
+		}
+		
+		public void run() {
+			log.info("Goting in A");
+			try {
+				waitPrint();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private class TaskB implements Runnable {
+		private TaskA taskA;
+		private TaskB(TaskA task) {
+			taskA = task;
+		}
+		public void run() {
+			log.info("Going in B");
+			OrnamentalGarden.sleep(1000);
+			awake();
+		}
+		
+		private synchronized void awake() {
+			log.info("Awake, please!");
+			taskA.awake();
+		}
+		
+	}
+	
+	/**
+	 * A进入任务,然后使用wait()挂起
+	 * B获取A对象,进入任务,等待,使用A对象的唤醒
+	 * 
+	 */
+	@Test
+	public void test3() {
+		ExecutorService exec = Executors.newCachedThreadPool();
+		TaskA a = new TaskA();
+		exec.execute(a);
+		exec.execute(new TaskB(a));
+		OrnamentalGarden.sleep(4000);
+		exec.shutdownNow();
+	}
+	
 	
 }
