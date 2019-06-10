b9b3f7770c4a4d3fc7a098c49cb786673b29b85a
fictio <ficito.d2k@gmail.com>
2019-06-10 18:09:10 +0800

    feat: add Collection stream test;#190610

---
+++ src/main/java/com/fictio/parrot/demo/CollectionTest.java
@@ -0,0 +1,80 @@
+package com.fictio.parrot.demo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+class Student {
+    public enum Sex  {
+        MALE,FEMALE
+    }
+    public Student(String name,Sex sex,LocalDate birth) {
+        this.name = name;
+        this.sex = sex;
+        this.birthday = birth;
+    }
+    String name;
+    Sex sex;
+    LocalDate birthday;
+}
+
+@Slf4j
+public class CollectionTest {
+    
+    private List<Student> stus;
+    
+    @Before
+    public void collectionsBuild() {
+        stus = new ArrayList<>();
+        stus.add(new Student("张三",Student.Sex.MALE,LocalDate.of(2000, 1, 1)));
+        stus.add(new Student("李四",Student.Sex.FEMALE,LocalDate.of(1990, 1, 1)));
+        stus.add(new Student("李四",Student.Sex.MALE,LocalDate.of(1993, 1, 1)));
+        stus.add(new Student("王五",Student.Sex.MALE,LocalDate.of(1991, 1, 1)));
+        stus.add(new Student("金唯",Student.Sex.FEMALE,LocalDate.of(1992, 1, 1)));
+        stus.add(new Student("金唯",Student.Sex.FEMALE,null));
+    }
+    
+    @Test
+    public void sortTest() {
+        if(stus == null || stus.isEmpty()) return;
+        stus = stus.stream().sorted(Comparator.comparing(Student::getBirthday).thenComparing(Student::getSex))
+                .collect(Collectors.toList());
+        stus.forEach(s->log.info("{}",s));
+    }
+    
+    @Test
+    public void distinctTest() {
+        if(stus == null || stus.isEmpty()) return;
+        stus = stus.stream().filter(distinctByKey(s->(s.getName()+s.getBirthday())))
+                .collect(Collectors.toList());
+        stus.forEach(s->log.info("{}",s));
+    }
+    
+    @Test
+    public void sortAndDistinctJoin() {
+        if(stus == null || stus.isEmpty()) return;
+        stus = stus.stream().sorted(Comparator.comparing(Student::getBirthday).thenComparing(Student::getSex).reversed())
+                .filter(distinctByKey(s->(s.getName())))
+                .collect(Collectors.toList());
+        stus.forEach(s->log.info("{}",s));
+    }
+    
+    public <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor){
+        Map<Object, Boolean> map = new ConcurrentHashMap<>();
+        return t->map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+
+}
