ebdec18c660473825d3fc9efad73fdc92d6096ee
hek <ficito.d2k@gmail.com>
2019-11-13 22:52:18 +0800

    feat: Impl in enums;#191113

---
+++ src/main/java/com/fictio/parrot/thinking/enums/RandomTest.java
@@ -0,0 +1,155 @@
+package com.fictio.parrot.thinking.enums;
+
+import java.util.Random;
+
+import org.junit.Test;
+import com.fictio.parrot.thinking.enums.RandomTest.Food.Coffee;
+import com.fictio.parrot.thinking.enums.RandomTest.Food.Dessert;
+
+import lombok.extern.slf4j.Slf4j;
+
+class EnumUtils {
+	private static Random rand = new Random(47);
+	// T<extends Enum<T>> 表示 T 是 Enum 的一个实例
+	public static <T extends Enum<T>> T random(Class<T> ec) {
+		return random(ec.getEnumConstants());
+	}
+	public static <T> T random(T[] values) {
+		return values[rand.nextInt(values.length)];
+	}
+}
+
+@Slf4j
+public class RandomTest {
+	enum Activity {
+		SITTING,LYING,STANDING,HOPPING,RUNNING,DODGING,JUMPING,FLYING
+	}
+
+	@Test
+	public void randTest() {
+		for(int i = 0; i < 10; i++)
+			log.debug("{}",EnumUtils.random(Activity.class));
+	}
+
+	// 接口组织枚举
+	interface Food {
+		enum Appetizer implements Food {
+			SALAD,SOUP,SPRING_ROLLS;
+		}
+		enum MainCourse implements Food {
+			LASAGNE, BURRITO, PAD_THAI,LENTILS,HUMMOUS,VINDALOO;
+		}
+		enum Dessert implements Food {
+			TIRAMISE,GELATO,BLACK_FOREST_CAKE,FRUIT,GREME_CARAMEL;
+		}
+		enum Coffee implements Food {
+			BLACK_COFFEE,DECAF_COFFEE,ESPRESSO,LATTE,CAPPUCCINO;
+		}
+	}	
+	
+	@Test
+	public void typeOfFoodTest() {
+		Food food = Dessert.BLACK_FOREST_CAKE;
+		food = Coffee.BLACK_COFFEE;
+		log.debug("FOOD: {}",food);
+	}
+
+	// "枚举的枚举"
+	enum Course {
+		APPETIZER(Food.Appetizer.class),
+		MAINCOUSE(Food.MainCourse.class),
+		DESSERT(Food.Dessert.class),
+		COFFEE(Food.Coffee.class);
+		private Food[] values;
+		private Course(Class<? extends Food> kind) {
+			values = kind.getEnumConstants();
+		}
+		public Food randomSelect() {
+			return EnumUtils.random(values);
+		}
+	}	
+	
+	// 拼凑菜单
+	@Test
+	public void mealTest() {
+		for(int i = 0; i < 5; i++) {
+			log.debug("=====start=====");
+			// 遍历Course 实现 枚举的枚举
+			for(Course c : Course.values()) {
+				Food food = c.randomSelect();
+				log.debug("FOOD == > {}",food);
+			}
+			log.debug("=====end=====");
+		}
+	}
+	
+	// 枚举嵌套枚举
+	enum SecurityCategory {
+		STOCK(Security.Stock.class),
+		BOND(Security.Bond.class);
+		private Security [] values;
+		private SecurityCategory (Class <? extends Security> kind) {
+			values = kind.getEnumConstants();
+		}
+		public Security randomSelect() {
+			return EnumUtils.random(values);
+		}
+		// 将接口组成一个公共类型
+		interface Security {
+			enum Stock implements Security { SHORT,LONG,MARGIN }
+			enum Bond implements Security { MUNICIPAL,JUNK }
+		}
+	}
+	
+	@Test
+	public void securityTest() {
+		for(int i = 0; i < 10; i++) {
+			SecurityCategory category = EnumUtils.random(SecurityCategory.class);
+			log.debug("{} : {}",category,category.randomSelect());
+		}
+	}
+	
+	// 只是重新组织了代码,目的是更清晰的结构
+	enum Course2 {
+		APPETIZER(Food.Appetizer.class),
+		MAINCOUSE(Food.MainCourse.class),
+		DESSERT(Food.Dessert.class),
+		COFFEE(Food.Coffee.class);
+		private Food[] values;
+		private Course2(Class<? extends Food> kind) {
+			values = kind.getEnumConstants();
+		}
+		public Food randomSelect() {
+			return EnumUtils.random(values);
+		}
+		public interface Food {
+			enum Appetizer implements Food {
+				SALAD,SOUP,SPRING_ROLLS;
+			}
+			enum MainCourse implements Food {
+				LASAGNE, BURRITO, PAD_THAI,LENTILS,HUMMOUS,VINDALOO;
+			}
+			enum Dessert implements Food {
+				TIRAMISE,GELATO,BLACK_FOREST_CAKE,FRUIT,GREME_CARAMEL;
+			}
+			enum Coffee implements Food {
+				BLACK_COFFEE,DECAF_COFFEE,ESPRESSO,LATTE,CAPPUCCINO;
+			}
+		}	
+	}		
+	
+	// 拼凑菜单2
+	@Test
+	public void meal2Test() {
+		for(int i = 0; i < 5; i++) {
+			log.debug("=====start=====");
+			// 遍历Course 实现 枚举的枚举
+			for(Course2 c : Course2.values()) {
+				Course2.Food food = c.randomSelect();
+				log.debug("FOOD2 == > {}",food);
+			}
+			log.debug("=====end=====");
+		}
+	}	
+	
+}
