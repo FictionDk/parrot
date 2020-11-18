f3a9a3756dfda4fd16ac514966e02d724ab179c1
fictio <ficito.d2k@gmail.com>
2020-11-18 14:08:24 +0800

    feat: Add aes and snowflake;#201118

---
+++ src/main/java/com/fictio/parrot/algorithm/SnowflakeUtil.java
@@ -0,0 +1,238 @@
+package com.fictio.parrot.algorithm;
+
+import java.net.NetworkInterface;
+import java.security.SecureRandom;
+import java.time.Instant;
+import java.util.Enumeration;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+class SnowflakeB {
+    /**
+     * 起始的时间戳
+     */
+    private final static long START_STMP = 1603944000000L;
+
+    /**
+     * 每一部分占用的位数
+     */
+    private final static long SEQUENCE_BIT = 12; //序列号占用的位数
+    private final static long MACHINE_BIT = 5;   //机器标识占用的位数
+    private final static long DATACENTER_BIT = 5;//数据中心占用的位数
+
+    /**
+     * 每一部分的最大值
+     */
+    private final static long MAX_DATACENTER_NUM = -1L ^ (-1L << DATACENTER_BIT);
+    private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
+    private final static long MAX_SEQUENCE = -1L ^ (-1L << SEQUENCE_BIT);
+
+    /**
+     * 每一部分向左的位移
+     */
+    private final static long MACHINE_LEFT = SEQUENCE_BIT;
+    private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
+    private final static long TIMESTMP_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
+
+    private long datacenterId;  //数据中心
+    private long machineId;     //机器标识
+    private long sequence = 0L; //序列号
+    private long lastStmp = -1L;//上一次时间戳
+
+    public SnowflakeB(long datacenterId, long machineId) {
+        if (datacenterId > MAX_DATACENTER_NUM || datacenterId < 0) {
+            throw new IllegalArgumentException("datacenterId can't be greater than MAX_DATACENTER_NUM or less than 0");
+        }
+        if (machineId > MAX_MACHINE_NUM || machineId < 0) {
+            throw new IllegalArgumentException("machineId can't be greater than MAX_MACHINE_NUM or less than 0");
+        }
+        this.datacenterId = datacenterId;
+        this.machineId = machineId;
+    }
+
+    /**
+     * 产生下一个ID
+     *
+     * @return
+     */
+    public synchronized long nextId() {
+        long currStmp = getNewstmp();
+        if (currStmp < lastStmp) {
+            throw new RuntimeException("Clock moved backwards.  Refusing to generate id");
+        }
+
+        if (currStmp == lastStmp) {
+            //相同毫秒内，序列号自增
+            sequence = (sequence + 1) & MAX_SEQUENCE;
+            //同一毫秒的序列数已经达到最大
+            if (sequence == 0L) {
+                currStmp = getNextMill();
+            }
+        } else {
+            //不同毫秒内，序列号置为0
+            sequence = 0L;
+        }
+
+        lastStmp = currStmp;
+
+        return (currStmp - START_STMP) << TIMESTMP_LEFT //时间戳部分
+                | datacenterId << DATACENTER_LEFT       //数据中心部分
+                | machineId << MACHINE_LEFT             //机器标识部分
+                | sequence;                             //序列号部分
+    }
+
+    private long getNextMill() {
+        long mill = getNewstmp();
+        while (mill <= lastStmp) {
+            mill = getNewstmp();
+        }
+        return mill;
+    }
+
+    private long getNewstmp() {
+        return System.currentTimeMillis();
+    }
+}
+
+
+class SnowflakeA {
+    //private static final int UNUSED_BITS = 1; // Sign bit, Unused (always set to 0)
+    private static final int EPOCH_BITS = 41;
+    private static final int NODE_ID_BITS = 5;
+    private static final int SEQUENCE_BITS = 12;
+
+    private static final long maxNodeId = (1L << NODE_ID_BITS) - 1;
+    private static final long maxSequence = (1L << SEQUENCE_BITS) - 1;
+
+    // Custom Epoch (2020-10-29T12:00:00+8:00)
+    private static final long DEFAULT_CUSTOM_EPOCH = 1603944000000L;
+
+    private final long nodeId;
+    private final long customEpoch;
+
+    private volatile long lastTimestamp = -1L;
+    private volatile long sequence = 0L;
+
+    // Create Snowflake with a nodeId and custom epoch
+    public SnowflakeA(long nodeId, long customEpoch) {
+        if(nodeId < 0 || nodeId > maxNodeId) {
+            throw new IllegalArgumentException(String.format("NodeId must be between %d and %d", 0, maxNodeId));
+        }
+        this.nodeId = nodeId;
+        this.customEpoch = customEpoch;
+    }
+
+    // Create Snowflake with a nodeId
+    public SnowflakeA(long nodeId) {
+        this(nodeId, DEFAULT_CUSTOM_EPOCH);
+    }
+
+    // Let Snowflake generate a nodeId
+    public SnowflakeA() {
+        this.nodeId = createNodeId();
+        this.customEpoch = DEFAULT_CUSTOM_EPOCH;
+    }
+
+    public synchronized long nextId() {
+        long currentTimestamp = timestamp();
+
+        if(currentTimestamp < lastTimestamp) {
+            throw new IllegalStateException("Invalid System Clock!");
+        }
+
+        if (currentTimestamp == lastTimestamp) {
+            sequence = (sequence + 1) & maxSequence;
+            if(sequence == 0) {
+                // Sequence Exhausted, wait till next millisecond.
+                currentTimestamp = waitNextMillis(currentTimestamp);
+            }
+        } else {
+            // reset sequence to start with zero for the next millisecond
+            sequence = 0;
+        }
+
+        lastTimestamp = currentTimestamp;
+
+        long id = currentTimestamp << (NODE_ID_BITS + SEQUENCE_BITS)
+                | (nodeId << SEQUENCE_BITS)
+                | sequence;
+
+        return id;
+    }
+
+
+    // Get current timestamp in milliseconds, adjust for the custom epoch.
+    private long timestamp() {
+        return Instant.now().toEpochMilli() - customEpoch;
+    }
+
+    // Block and wait till next millisecond
+    private long waitNextMillis(long currentTimestamp) {
+        while (currentTimestamp == lastTimestamp) {
+            currentTimestamp = timestamp();
+        }
+        return currentTimestamp;
+    }
+
+    private long createNodeId() {
+        long nodeId;
+        try {
+            StringBuilder sb = new StringBuilder();
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                byte[] mac = networkInterface.getHardwareAddress();
+                if (mac != null) {
+                    for(byte macPort: mac) {
+                        sb.append(String.format("%02X", macPort));
+                    }
+                }
+            }
+            nodeId = sb.toString().hashCode();
+        } catch (Exception ex) {
+            nodeId = (new SecureRandom().nextInt());
+        }
+        nodeId = nodeId & maxNodeId;
+        return nodeId;
+    }
+
+    public long[] parse(long id) {
+        long maskNodeId = ((1L << NODE_ID_BITS) - 1) << SEQUENCE_BITS;
+        long maskSequence = (1L << SEQUENCE_BITS) - 1;
+
+        long timestamp = (id >> (NODE_ID_BITS + SEQUENCE_BITS)) + customEpoch;
+        long nodeId = (id & maskNodeId) >> SEQUENCE_BITS;
+        long sequence = id & maskSequence;
+
+        return new long[]{timestamp, nodeId, sequence};
+    }
+
+    @Override
+    public String toString() {
+        return "Snowflake Settings [EPOCH_BITS=" + EPOCH_BITS + ", NODE_ID_BITS=" + NODE_ID_BITS
+                + ", SEQUENCE_BITS=" + SEQUENCE_BITS + ", CUSTOM_EPOCH=" + customEpoch
+                + ", NodeId=" + nodeId + "]";
+    }
+}
+
+@Slf4j
+public class SnowflakeUtil {
+
+    @Test
+    public void test() {
+        SnowflakeA a = new SnowflakeA();
+        Long tmp = a.nextId();
+        log.debug("{}",tmp);
+        log.debug("{}",a.parse(tmp));
+
+        SnowflakeB b = new SnowflakeB(0, 2);
+        tmp = b.nextId();
+        log.debug("{}",b.nextId());
+        log.debug("{}",a.parse(tmp));
+
+        for(int i = 0; i < 138; i++)
+            System.out.println(a.nextId());
+    }
+}
