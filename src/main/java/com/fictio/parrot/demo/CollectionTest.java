1a3cb8fbbc3eb9f5925bc4fa10ca9c59b3efb60e
fictio <ficito.d2k@gmail.com>
2020-11-18 14:15:38 +0800

    feat: collections utils;#201118

--- src/main/java/com/fictio/parrot/demo/CollectionTest.java
+++ src/main/java/com/fictio/parrot/demo/CollectionTest.java
@@ -28,35 +28,41 @@ class Student {
         this.sex = sex;
         this.birthday = birth;
     }
+    public Student setGrade(Integer g) {
+        this.grade = g;
+        return this;
+    }
     String name;
     Sex sex;
     LocalDate birthday;
+    Integer grade;
 }
 
 @Slf4j
 public class CollectionTest {
-    
+
     private List<Student> stus;
-    
+
     @Before
     public void collectionsBuild() {
         stus = new ArrayList<>();
-        stus.add(new Student("张三",Student.Sex.MALE,LocalDate.of(2000, 1, 1)));
-        stus.add(new Student("李四",Student.Sex.FEMALE,LocalDate.of(1990, 1, 1)));
-        stus.add(new Student("李四",Student.Sex.MALE,LocalDate.of(1993, 1, 1)));
-        stus.add(new Student("王五",Student.Sex.MALE,LocalDate.of(1991, 1, 1)));
-        stus.add(new Student("金唯",Student.Sex.FEMALE,LocalDate.of(1992, 1, 1)));
-        stus.add(new Student("金唯",Student.Sex.FEMALE,null));
+        stus.add(new Student("张三",Student.Sex.MALE,LocalDate.of(2000, 1, 1)).setGrade(90));
+        stus.add(new Student("李四",Student.Sex.FEMALE,LocalDate.of(1990, 1, 1)).setGrade(80));
+        stus.add(new Student("李四",Student.Sex.MALE,LocalDate.of(1993, 1, 1)).setGrade(55));
+        stus.add(new Student("王五",Student.Sex.MALE,LocalDate.of(1991, 1, 1)).setGrade(34));
+        stus.add(new Student("金唯",Student.Sex.FEMALE,LocalDate.of(1992, 1, 1)).setGrade(60));
+        //stus.add(new Student("金唯",Student.Sex.FEMALE,null));
     }
-    
+
     @Test
     public void sortTest() {
         if(stus == null || stus.isEmpty()) return;
-        stus = stus.stream().sorted(Comparator.comparing(Student::getBirthday).thenComparing(Student::getSex))
+        log.debug("{}",stus.stream().sorted(Comparator.comparing(Student::getGrade)).mapToInt(Student::getGrade).findFirst());
+        stus = stus.stream().sorted(Comparator.comparing(Student::getGrade).thenComparing(Student::getSex))
                 .collect(Collectors.toList());
         stus.forEach(s->log.info("{}",s));
     }
-    
+
     @Test
     public void distinctTest() {
         if(stus == null || stus.isEmpty()) return;
@@ -64,7 +70,7 @@ public class CollectionTest {
                 .collect(Collectors.toList());
         stus.forEach(s->log.info("{}",s));
     }
-    
+
     @Test
     public void sortAndDistinctJoin() {
         if(stus == null || stus.isEmpty()) return;
@@ -73,25 +79,25 @@ public class CollectionTest {
                 .collect(Collectors.toList());
         stus.forEach(s->log.info("{}",s));
     }
-    
+
     @Test
     public void group() {
         if(stus == null || stus.isEmpty()) return;
         Map<Sex,List<Student>> stuMap = stus.stream().collect(Collectors.groupingBy(Student::getSex));
         stuMap.forEach((k,v)->log.info("{} | {}",k,v));
     }
-    
+
     public <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor){
         Map<Object, Boolean> map = new ConcurrentHashMap<>();
         return t->map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
     }
 
     @Test
-    public void listAddTest() {
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < 20 ; i++) {
-            list.add(1);
+    public void foreachTest() {
+        for(Student s : stus) {
+            if("张三".equals(s.getName())) s.setBirthday(s.getBirthday().plusMonths(2));
         }
+        stus.stream().forEach(System.out::println);
     }
-    
+
 }
