f1cd86da6e5046078a61c56e33d1883e49b576c2
fictio <ficito.d2k@gmail.com>
2021-06-08 17:54:49 +0800

    fix: jdk proxy supple;#210608

--- src/main/java/com/fictio/parrot/logic/proxy/JDKDynamicProxyDemo.java
+++ src/main/java/com/fictio/parrot/logic/proxy/JDKDynamicProxyDemo.java
@@ -7,24 +7,24 @@ import java.lang.reflect.Proxy;
 import org.junit.Test;
 
 public class JDKDynamicProxyDemo {
-    
+
     static interface IService {
         public void sayHello();
     }
-    
+
     static class RealService implements IService {
         @Override
         public void sayHello() {
             System.out.println("real Hello");
         }
     }
-    
+
     static class SimpleInvocationHandler implements InvocationHandler {
         private Object realObj;
         public SimpleInvocationHandler(Object realObj) {
             this.realObj = realObj;
         }
-        
+
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             System.out.println("entering "+method.getName());
@@ -33,21 +33,34 @@ public class JDKDynamicProxyDemo {
             return result;
         }
     }
-    
+
     /**
      * newProxyInstance(ClassLoader loader,class<?>[] interfaces, InvocationHandler h)
      * loader 类加载器
      * interfaces 代理要实现的接口列表
      * handler 代理方法的实际调用者,继承InvocationHandler接口并实现
-     * 
+     *
      * return Object: 可以强制转换未interfaces数组中的某个接口类型,注意的是不能强转为实现类的类型
      */
     @Test
     public void demo() {
         IService realService = new RealService();
         IService proxyService = (IService) Proxy.newProxyInstance(
-                IService.class.getClassLoader(), new Class<?>[] {IService.class}, 
+                IService.class.getClassLoader(), new Class<?>[] {IService.class},
                 new SimpleInvocationHandler(realService));
+        proxyService.sayHello();
+
+        proxyService = (IService) Proxy.newProxyInstance(JDKDynamicProxyDemo.class.getClassLoader(),
+                new Class<?>[] {IService.class},
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println("in_entering "+method.getName());
+                        Object result = method.invoke(realService, args);
+                        System.out.println("in_leaving "+method.getName());
+                        return result;
+                    }
+                });
         proxyService.sayHello();
     }
 
